bf49e8e4fb04092898e97edf3ae2e9593cc36aa1
George Wilson <george@g-wilson.co.uk>
2020-09-24 16:08:07 +0100

    remove name and format reviewdog params

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,7 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 semgrep --config="${INPUT_SEMGREP_CONFIG}" --sarif ${INPUT_SEMGREP_TARGET} \
-  | reviewdog -f=sarif \
-      -name="semgrep" \
+  | reviewdog \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
