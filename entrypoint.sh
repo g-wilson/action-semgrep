0b2bec4a7e09213b5c26efdf7f21fa689c9fe48c
George Wilson <george@g-wilson.co.uk>
2020-09-25 13:17:50 +0100

    restore tool name argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ semgrep --config="${INPUT_SEMGREP_CONFIG}" --json ${INPUT_SEMGREP_TARGET} \
   | jq -r '.results[] | "\(.extra.severity[0:1]):\(.path):\(.start.line) \(.extra.message)"' \
   | reviewdog \
       -efm="%t:%f:%l %m" \
+      -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
