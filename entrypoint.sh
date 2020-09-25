e6585b55cccd48f242e61789438efe05303173f5
George Wilson <george@g-wilson.co.uk>
2020-09-25 12:03:30 +0100

    add severity

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 semgrep --config="${INPUT_SEMGREP_CONFIG}" --json ${INPUT_SEMGREP_TARGET} \
-  | jq -r '.results[] | "\(.path):\(.start.line) \(.extra.message)"' \
+  | jq -r '.results[] | "\(.extra.severity[0:1]):\(.path):\(.start.line) \(.extra.message)"' \
   | reviewdog \
-      -efm="%f:%l %m" \
+      -efm="%t:%f:%l %m" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
